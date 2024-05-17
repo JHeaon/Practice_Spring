c9fb5bdaccdcfce723c5ff642c98202f0dd1dca6
JHeaon <j3heawon@gmail.com>
2024-05-17 09:48:33 +0900

    Feat: Entity 작성

---
+++ src/main/groovy/com/sparta/practice_spring/entity/Schedule.java
@@ -0,0 +1,40 @@
+package com.sparta.practice_spring.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Time;
+
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
+@Getter
+@Setter
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
+@NoArgsConstructor
+public class Schedule extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "contents", nullable = false)
+    private String contents;
+
+    @Column(name = "manager", nullable = false)
+    private String manager;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+
+
+
+
+
+
+
+}
+
