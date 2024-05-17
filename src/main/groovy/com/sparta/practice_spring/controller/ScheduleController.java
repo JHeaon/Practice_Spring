fe9f08a288935a39f34b1abbb8d816a48f9d4895
JHeaon <j3heawon@gmail.com>
2024-05-17 11:08:36 +0900

    Fix: 오타 수정

--- src/main/groovy/com/sparta/practice_spring/controller/ScheduleController.java
+++ src/main/groovy/com/sparta/practice_spring/controller/ScheduleController.java
@@ -16,26 +16,22 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @Autowired
-    public ScheduleController(ScheduleService scheduleService){
+    public ScheduleController(ScheduleService scheduleService) {
         this.scheduleService = scheduleService;
     }
 
     @PostMapping("/schedules")
-    public ScheduleReponseDto createSchedule(@RequestBody ScheduleRequestDto requestDto){
+    public ScheduleReponseDto createSchedule(@RequestBody ScheduleRequestDto requestDto) {
         return scheduleService.createSchedule(requestDto);
     }
 
     @GetMapping("/schedules")
-    public List<ScheduleReponseDto> getSchedules(){
+    public List<ScheduleReponseDto> getSchedules() {
         return scheduleService.getSchedules();
     }
+
     @PutMapping("/schedules/{id}")
-    public Long updateSchedules(@PathVariable Long id, @RequestBody ScheduleRequestDto scheduleRequestDto){
+    public Long updateSchedule(@PathVariable Long id, @RequestBody ScheduleRequestDto scheduleRequestDto) {
         return scheduleService.updateSchedules(id, scheduleRequestDto);
     }
-
-
-
-
-
 }
