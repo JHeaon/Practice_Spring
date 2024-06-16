d9055180042a7294a79482774a7aa45a09ca3c4c
JHeaon <j3heawon@gmail.com>
2024-06-17 01:02:18 +0900

    Remove: 프로젝트 초기화

--- src/main/groovy/com/sparta/practice_spring/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.practice_spring.repository;
-
-import com.sparta.practice_spring.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
